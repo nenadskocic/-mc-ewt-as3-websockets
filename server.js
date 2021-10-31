5aa8f93e394451d04da14799f299d3298ea9916a
Nenad Skocic <nenad.skocic@gmail.com>
2021-10-31 13:54:46 -0400

    Back-end complete.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const { clearTimeout } = require('timers');
-const { setTimeout } = require('timers/promises');
-
+/*
+    "StAuth10222: I Nenad Skocic, 000107650 certify that this material is my original work. No other person's work has been used 
+    without due acknowledgement. I have not made my work available to anyone else."
+*/
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -13,22 +14,45 @@ app.get('/bidder', function(req,res){
     res.sendFile(__dirname + '/bidder.html');
 });
 
+
 io.on('connection', function(socket) {
+
+    let biddersBidsArr = [];
+    let counter = 0;
+
+    socket.on('bidderRegister', function(data) {
+        console.log("Bidder: " + data.bidderName + " has registered.");
+
+        io.emit("bidderRegister", "Auction has participants");
+    });
+
     socket.on('auctionContent', function(data) {
         console.log("Auction has started!", data);
 
+        io.emit('auctionContent', data)
         var timer = data.time;
         
         var timeout = setInterval(() => {
-            socket.emit('timer', timer);
+            io.emit('timer', timer);
             timer--;
 
             if(timer === 0) {
-                socket.emit('timer', "Auction is over");
+                io.emit('timer', "Auction is over");
                 clearInterval(timeout);
+            } else {
             }
         }, 1000);
-    })      
+    });
+
+    socket.on('bidderBid', function(data) {
+        counter++;
+        biddersBidsArr.push({ bidderID: data.bidderID, bidderName: data.bidderName, bidderPrice: data.bidderPrice, counter: counter });
+        io.emit("bidderBid", biddersBidsArr );
+    });
+
+    socket.on('disconnect', function() {
+        biddersBidsArr = [];
+    });
 });
 
 http.listen(3000, function() {
